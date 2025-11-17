2ce97e56e2aea9c29b5d9cdbb47a983681f14a82
Elias Kristoffer Grønn Moen <ekmoen@uia.no>
2025-11-17 14:33:34 +0100

    Fixed assignment 4 to align and copied assignment 5 code.

--- assignment_4/main.py
+++ assignment_4/main.py
@@ -5,8 +5,8 @@ import numpy as np
 dirname = os.path.dirname(__file__)
 
 #1
-def harris_corner_detection(referance_image, toFile = True):
-    img = referance_image.copy()
+def harris_corner_detection(reference_image, toFile = True):
+    img = reference_image.copy()
     gray = np.float32(cv2.cvtColor(img,cv2.COLOR_BGR2GRAY))
 
     dst = cv2.cornerHarris(gray,2,3,0.04)
@@ -17,51 +17,60 @@ def harris_corner_detection(referance_image, toFile = True):
     if (toFile):
         cv2.imwrite(dirname+"/images/harris_corner_detection.png",img)
     else:
-        cv2.imshow("corner detection",img)
+        #cv2.imshow("corner detection",img)
         cv2.waitKey(0)
         return max(dst,key=lambda item: item[0])
 
 #2 
 # Wanted to use SURF since it was not used in the figureprint task.
-# However, because it is patented and marked as nonfree, SIFT was used instead.
-def align_imgs_SIFT(image_to_align, referance_image, max_features, good_match_percent):
-    if image_to_align is None or referance_image is None: 
-        print('Image(s) not found')
-        return
-    
-    # Find keypoints and descriptors
-    detector = cv2.SIFT.create(max_features)
-    kp1, desc1 = detector.detectAndCompute(image_to_align, None)
-    kp2, desc2 = detector.detectAndCompute(referance_image, None)
-    
-    flann_index_kdtree = 1
-    index_parameters = dict(algorithm = flann_index_kdtree, trees = 5)
-    search_parameters = dict(checks=50)
-    
-    matcher = cv2.FlannBasedMatcher(index_parameters, search_parameters)
-    knn_matches = matcher.knnMatch(desc1, desc2, 2)
-    
-    good_matches = []
-    for m,n in knn_matches:
-        if m.distance < good_match_percent * n.distance:
-            good_matches.append(m)
-    
-    img_matches = np.empty((max(image_to_align.shape[0], referance_image.shape[0]), image_to_align.shape[1]+referance_image.shape[1], 3), dtype=np.uint8)
-    cv2.drawMatches(image_to_align, 
-                    kp1, 
-                    referance_image, kp2, 
-                    good_matches, 
-                    img_matches, 
-                    flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    
-    cv2.imwrite(dirname+"/images/SIFT_aligned_with_min_acc={}%.png".format(good_match_percent),img_matches)
+# However, because it is patented and marked as nonfree, I did not use it
+# Sift reference code had no implementation of alignment, only matching, so I used ORB
 
-referance_image = cv2.imread(dirname + "/reference_img.png")
+def align_imgs_ORB(image_to_align, reference_image, max_features, good_match_percent):
+
+    align_gray = cv2.cvtColor(image_to_align, cv2.COLOR_BGR2GRAY)
+    referance_gray = cv2.cvtColor(reference_image, cv2.COLOR_BGR2GRAY)
+    
+    # Detect ORB features and compute descriptors.
+    orb = cv2.ORB_create(max_features)
+    kp1, desc1 = orb.detectAndCompute(align_gray, None)
+    kp2, desc2 = orb.detectAndCompute(referance_gray, None)
+    
+    # Create brute force matcher and match with ORB descriptors
+    matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+    matches = matcher.match(desc1, desc2, None)
+    
+    # Sort matches by accuracy
+    matches = sorted(matches, key=lambda x: x.distance, reverse=False)
+
+    # Only keep best matches 
+    matchesToKeep = int(len(matches) * good_match_percent)
+    matches = matches[:matchesToKeep]
+
+    # Draw matches and save to image
+    imMatches = cv2.drawMatches(image_to_align, kp1, reference_image, kp2, matches, None)
+    cv2.imwrite(dirname+"/images/matches.jpg", imMatches)
+    
+    # Create empty matrix and fill with x,y coordinates of matches
+    points1 = np.zeros((len(matches), 2), dtype=np.float32)
+    points2 = np.zeros((len(matches), 2), dtype=np.float32)
+    for i, match in enumerate(matches):
+        points1[i, :] = kp1[match.queryIdx].pt
+        points2[i, :] = kp2[match.trainIdx].pt
+    
+    # Calculate how to warp, then align image
+    M, _ = cv2.findHomography(points1, points2, cv2.RANSAC)
+    height, width, _ = reference_image.shape
+    warped_image = cv2.warpPerspective(image_to_align, M, (width, height))
+    
+    cv2.imwrite(dirname+"/images/aligned_img.png", warped_image)
+
+reference_image = cv2.imread(dirname + "/reference_img.png")
 align_image = cv2.imread(dirname + "/align_this.jpg")
 
-harris_corner_detection(referance_image) 
+harris_corner_detection(reference_image) 
 harris_corner_detection(align_image,False)
+cv2.destroyAllWindows()
 
-
-align_imgs_SIFT(align_image,referance_image,100,75)
+align_imgs_ORB(align_image,reference_image,10000,0.1)
 
---
+++ assignment_4/stinky_sift.py
@@ -0,0 +1,66 @@
+import cv2
+import os
+import numpy as np
+
+dirname = os.path.dirname(__file__)
+
+#1
+def harris_corner_detection(referance_image, toFile = True):
+    img = referance_image.copy()
+    gray = np.float32(cv2.cvtColor(img,cv2.COLOR_BGR2GRAY))
+
+    dst = cv2.cornerHarris(gray,2,3,0.04)
+    dst = cv2.dilate(dst,None)
+    
+    img[dst>0.01*dst.max()]=[0,0,255]
+    
+    if (toFile):
+        cv2.imwrite(dirname+"/images/harris_corner_detection.png",img)
+    else:
+        cv2.imshow("corner detection",img)
+        cv2.waitKey(0)
+        return max(dst,key=lambda item: item[0])
+
+# 2 
+# Wanted to use SURF since it was not used in the figureprint task.
+# However, because it is patented and marked as nonfree, SIFT was used instead.
+def match_imgs_SIFT(image_to_align, referance_image, max_features, good_match_percent):
+    if image_to_align is None or referance_image is None: 
+        print('Image(s) not found')
+        return
+    
+    # Find keypoints and descriptors
+    detector = cv2.SIFT.create(max_features)
+    kp1, desc1 = detector.detectAndCompute(image_to_align, None)
+    kp2, desc2 = detector.detectAndCompute(referance_image, None)
+    
+    flann_index_kdtree = 1
+    index_parameters = dict(algorithm = flann_index_kdtree, trees = 5)
+    search_parameters = dict(checks=50)
+    
+    matcher = cv2.FlannBasedMatcher(index_parameters, search_parameters)
+    knn_matches = matcher.knnMatch(desc1, desc2, 2)
+    
+    good_matches = []
+    for m,n in knn_matches:
+        if m.distance < good_match_percent * n.distance:
+            good_matches.append(m)
+    
+    img_matches = np.empty((max(image_to_align.shape[0], referance_image.shape[0]), image_to_align.shape[1]+referance_image.shape[1], 3), dtype=np.uint8)
+    cv2.drawMatches(image_to_align, 
+                    kp1, 
+                    referance_image, kp2, 
+                    good_matches, 
+                    img_matches, 
+                    flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    
+    cv2.imwrite(dirname+"/images/SIFT_matched_with_min_acc={}%.png".format(good_match_percent),img_matches)
+
+referance_image = cv2.imread(dirname + "/reference_img.png")
+align_image = cv2.imread(dirname + "/align_this.jpg")
+
+harris_corner_detection(referance_image) 
+harris_corner_detection(align_image,False)
+cv2.destroyAllWindows()
+
+match_imgs_SIFT(align_image,referance_image,1000,85)
